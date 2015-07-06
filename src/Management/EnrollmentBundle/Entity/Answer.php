f9270bbd51f9aabc50d14a5ca786ec580d1ae1f0
Natalia Stanko <anithaly@gmail.com>
2015-07-06 17:10:50 +0200

    add enrollment entities

---
+++ src/Management/EnrollmentBundle/Entity/Answer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Management\EnrollmentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Answer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Answer
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
