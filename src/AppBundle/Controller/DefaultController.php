b232e16973e47b667affa937fdf31d2d0cf84e0e
Natalia Stanko <anithaly@gmail.com>
2015-07-12 16:34:25 +0200

    use locale from Accept-Language header

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,5 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+
 }
---
+++ src/AppBundle/Services/LocaleListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Services;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Making the Locale "Sticky" during a User's Session
+ * @link http://symfony.com/doc/current/cookbook/session/locale_sticky_session.html
+ */
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+
+            // if no explicit locale has been set on this request, use the Accept-Language header
+            $request->setLocale($request->getSession()->get(
+                '_locale',
+                $request->getPreferredLanguage(['en', 'pl'])
+                )
+            );
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+}
