7fb05000e2eefffa83dd1175c36a2e82149a409e
Natalia Stanko <anithaly@gmail.com>
2015-07-14 18:07:05 +0200

    add FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+
             new AppBundle\AppBundle(),
             new Management\EnrollmentBundle\ManagementEnrollmentBundle(),
+            new Management\UserBundle\ManagementUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Management/UserBundle/ManagementUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Management\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ManagementUserBundle extends Bundle
+{
+}
