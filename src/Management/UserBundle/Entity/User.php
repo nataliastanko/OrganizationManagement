fe1c18f5209f8b6e4e4ef4a20836d39a778125a0
Natalia Stanko <anithaly@gmail.com>
2015-07-15 22:40:24 +0200

    add user phone number field

--- src/Management/UserBundle/Entity/User.php
+++ src/Management/UserBundle/Entity/User.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace Management\UserBundle\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
+ * @UniqueEntity(fields="email", message="error.user.email.taken")
  */
 class User extends BaseUser
 {
@@ -16,6 +21,41 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(name="phone_number", type="string", nullable=true)
+     * Assert\NotBlank(message = "error.user.phone_number.not_blank", groups={"settings"})
+     * @Assert\Regex(
+     *  pattern= "/([0-9\-\+\s])?/",
+     *  match = true,
+     *  message = "error.user.phone_number.regex_not_match",
+     *  groups={"settings"}
+     * )
+     */
+    protected $phoneNumber;
+
+    /**
+     * Set phoneNumber
+     *
+     * @param string $phoneNumber
+     *
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
 
     public function __construct()
     {
