4377670203d98e352f47aa5db177d7aceb1382fa
Natalia Stanko <anithaly@gmail.com>
2015-07-16 03:00:35 +0200

    add meetings topics

---
+++ src/Management/MeetingBundle/Form/TopicType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Management\MeetingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TopicType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text',
+                [
+                'required' => true,
+                'label' => 'meeting.topic.name'
+                ]
+            )
+            ->add('description', 'textarea',
+                [
+                'required' => false,
+                'label' => 'meeting.topic.description'
+                ]
+            )
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Management\MeetingBundle\Entity\Topic'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'topic';
+    }
+}
